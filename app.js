aadc4157a51cba512f1e77cd48fe0f5c1c058237
Alfred Delle <alfred.delle@yale.edu>
2020-01-17 12:06:24 +0000

    File server with error handling

--- app.js
+++ app.js
@@ -6,16 +6,33 @@ const morgan = require('morgan')
 const app = express();
 
 //Logging middleware
-app.use(morgan("short"))
+app.use(morgan("short"));
 
 //Middleware to serve file if file exists
-const staticFilePath = path.join(__dirname, "static")
-app.use(express.static(staticFilePath))
+app.use(function (request, response, next) {
+  const filePath = path.join(__dirname, "static", res.url)
+  response.sendFile(filePath, function(err) {
+    if (err) {
+      next(new Error("Error sending file"));
+    }
+  })
+})
+
+//error handling middleware
+app.use(function(err, request, response, next) {
+  console.error(err);
+  next(err);
+})
+
+app.use(function(err, request, response, next) {
+  response.status(500);
+  response.send("Internal server error");
+})
 
 //404 Middleware
 app.use(function(request, response) {
   response.status(404);
-  response.send("File not found")
+  response.send("File not found");
 })
 
 app.listen(3000, function() {
